0c0c92267ddf64572da574106802eedca305cb1e
ruu23 <arwamah777@gmail.com>
2024-03-09 21:05:17 +0200

    base

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.update_at = datetime.now()
+
+    def save(self):
+        """"""
+        """"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """"""
+        """"""
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["crated_at"] = self.created_at.isoformat()
+        inst_dict["update_at"] = self.update_at.isoformat()
+        return inst_dict
+
+    def __str__(self):
+        """"""
+        """"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+
+if __name__ == "__main__":
+
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
